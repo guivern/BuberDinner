64eae10f55586d87859b3b59d5d7ee7b84f2db0a
Guillermo Verón <guiver95@gmail.com>
2022-08-29 12:10:31 -0400

    add jwt token generator

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddApplication().AddInfrastructure();
+builder.Services.AddApplication().AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
--- BuberDinner.Application/Services/Authtentication/AuthtenticationService.cs
+++ BuberDinner.Application/Services/Authtentication/AuthtenticationService.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BuberDinner.Application.Common.Interfaces.Authentication;
 
 namespace BuberDinner.Application.Services.Authtentication
 {
     public class AuthtenticationService : IAuthenticationService
     {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthtenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
         public AuthenticationResult Login(string email, string password)
         {
             return new AuthenticationResult(Guid.NewGuid(), "firstName", "lastName", email, "token");
@@ -14,7 +22,16 @@ namespace BuberDinner.Application.Services.Authtentication
 
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
-            return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
+            // check if user already exits
+
+            // create user
+
+            // generate jwt token
+            var userId = Guid.NewGuid();
+
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+            return new AuthenticationResult(userId, firstName, lastName, email, token);
         }
     }
 }
